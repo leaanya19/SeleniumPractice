f40ea61ec52ad4a5b32a94be9245a13c7bd5ea1f
leaanya19 <caamel579@gmail.com>
2020-11-18 01:16:55 -0500

    initial selenium codes

---
+++ src/main/java/com/peoplentech/seleniumpractice/Selenium1.java
@@ -0,0 +1,20 @@
+package com.peoplentech.seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium1 {
+    public static void main(String[] args) throws InterruptedException {
+        //set uo data as chrome driver
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        //create object for chrome driver
+        WebDriver driver = new ChromeDriver();
+        //tell chrome driver to go to url...
+        driver.get("https://www.google.com");
+
+        //wait 5 sec
+        Thread.sleep(5000);
+        //close the chrome
+        driver.close();
+    }
+}
